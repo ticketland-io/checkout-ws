09691129e30e4b0b15b88b63b5e2dcc87dd8199c
ppoliani <ppoliani@gmail.com>
2022-11-04 16:54:30 +0200

    chore: simplify new_checkout_session

--- src/services/checkout_manager_queue.rs
+++ src/services/checkout_manager_queue.rs
@@ -2,7 +2,6 @@ use eyre::Result;
 use borsh::{BorshSerialize};
 use amqp_helpers::producer::retry_producer::RetryProducer;
 use fiat_checkout_manager::models::create_checkout::CreateCheckout;
-use crate::ws::types::WsMethod;
 
 pub struct CheckoutManagerQueue {
   checkout_manager_producer: RetryProducer,
@@ -26,36 +25,11 @@ impl CheckoutManagerQueue {
     }
   }
 
-  pub async fn new_checkout_session(&self, create_checkout_session: WsMethod) -> Result<()> {
-    if let WsMethod::CreateCheckoutSession {
-      buyer_uid,
-      event_id,
-      sale_account,
-      ticket_nft,
-      ticket_type_index,
-      recipient,
-      seat_index,
-      seat_name,
-      ..
-    } = create_checkout_session {
-      let msg = CreateCheckout {
-        buyer_uid,
-        event_id,
-        sale_account,
-        ticket_nft,
-        ticket_type_index,
-        recipient,
-        seat_index,
-        seat_name,
-      };
-      
-      self.checkout_manager_producer.publish(
-        &"checkout_session",
-        &"checkout_session.new",
-        &msg.try_to_vec().unwrap()
-      ).await
-    } else {
-      panic!("Create session method should be used")
-    }
+  pub async fn new_checkout_session(&self, msg: CreateCheckout) -> Result<()> {
+    self.checkout_manager_producer.publish(
+      &"checkout_session",
+      &"checkout_session.new",
+      &msg.try_to_vec().unwrap()
+    ).await
   }
 }
