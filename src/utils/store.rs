acd0c48ab7fdf61409c6d00f41d05376bb482734
ppoliani <ppoliani@gmail.com>
2022-12-22 12:08:02 +0200

    fix: remove neo4j

--- src/utils/store.rs
+++ src/utils/store.rs
@@ -1,8 +1,4 @@
 use std::sync::Arc;
-use actix::prelude::*;
-use ticketland_core::{
-  actor::neo4j::Neo4jActor,
-};
 use crate::{
   ws::auth::AuthGuard,
   services::payment_manager_producer::PaymentManagerProducer,
@@ -11,7 +7,6 @@ use super::config::Config;
 
 pub struct Store {
   pub config: Config,
-  pub neo4j: Arc<Addr<Neo4jActor>>,
   pub auth_guard: Arc<AuthGuard>,
   pub payment_producer: PaymentManagerProducer,
 }
@@ -19,18 +14,6 @@ pub struct Store {
 impl Store {
   pub async fn new() -> Self {
     let config = Config::new().unwrap();
-
-    let neo4j = Arc::new(
-      Neo4jActor::new(
-        config.neo4j_host.clone(),
-        config.neo4j_domain.clone(),
-        config.neo4j_username.clone(),
-        config.neo4j_password.clone(),
-        config.neo4j_database.clone(),
-      )
-      .await
-      .start(),
-    );
 
     let auth_guard = Arc::new(AuthGuard::new(config.firebase_auth_key.clone()));
 
@@ -41,7 +24,6 @@ impl Store {
 
     Self {
       config,
-      neo4j,
       auth_guard,
       payment_producer,
     }
